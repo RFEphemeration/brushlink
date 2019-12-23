a6a3b5f22216e8e46e36512db5e8d747df1474ae
Robinson Farrar <robinson.farrar@gmail.com>
2019-12-23 12:20:45 -0500

    market simulation with random picks seems to be working

--- experiments/deckbuilding_metagame_market.py
+++ experiments/deckbuilding_metagame_market.py
@@ -1,87 +1,112 @@
+from __future__ import division
 from collections import namedtuple
 import random
 
 wallet_initial = 100.0
 total_unit_cost = 500.0
-total_units = 50
+total_units = 25
 total_players = 1000
+
+seasons = 100
+seasons_to_print = 1
 
 min_unit_cost = 1.0
 max_starting_unit_cost = 35.0
 max_unit_valuation = 75.0
 
-# could have unit archetypes for how players choose them
-# true rating understood by everyone of either high value or low value
-# divisive unit with some people thinking high value and others low value
-# with some ratio between the two groups of people
-# true rating, percentage underestimating, correct, over
+def debug_print(value):
+	#print(value)
+	pass
 
-archetypes = {
-	"Basic": [.1, .8, .1],
-	"Divisive": [.4, .1, .4],
-	"Overestimated": [.1, .5, .4],
-	"Underestimated": [.4, .5, .1]
-}
 
 class Unit:
-	def __init__(self, id, archetype):
+	
+	# could have unit archetypes for how players choose them
+	# divisive unit with some people thinking high value and others low value
+	# true rating understood by everyone of either high value or low value
+	# with some ratio between the two groups of people
+	# true rating, percentage underestimating, correct, over
+	archetypes = {
+		"Basic": (.1, .8, .1),
+		"Divisive": (.4, .1, .4),
+		"Overestimated": (.1, .5, .4),
+		"Underestimated": (.4, .5, .1)
+	}
+
+	def __init__(self, id):
 		self.id = id
-		self.archetype = archetype
+		self.archetype = random.choice(list(Unit.archetypes.keys()))
 		self.value = random.gauss(10.0, 5.0)
-		self.cost = random.randrange(min_unit_cost, max_starting_unit_cost)
+		self.cost = random.uniform(min_unit_cost, max_starting_unit_cost)
 		if (self.value < min_unit_cost):
 			self.value = min_unit_cost
 		self.uses = 0
 		self.cost_history = []
 		self.usage_rate_history = []
 
-
-units = [ Unit(id=id) for id in range(total_units)]
-
-
-def normalize_unit_costs():
-	for unit in units:
-		if unit.cost < min_unit_cost:
-			unit.cost = min_unit_cost
-	total_unit_cost_actual = sum([unit.cost for unit in units])
-	ratio = total_unit_cost_actual / total_unit_cost
-	extra_to_remove = 0
-	if ratio > 0.999 and ratio < 1.001:
-		return
-	for unit in units:
-		new_cost = unit.cost * ratio
-		if new_cost < min_unit_cost:
-			extra_to_remove = min_unit_cost - new_cost 
-			new_cost = min_unit_cost
-		unit.cost = new_cost
-	while extra_to_remove > min_unit_cost:
-		unit = random.choice(units)
-		if unit.cost > min_unit_cost:
-			new_cost = unit.cost - min_unit_cost
+	@staticmethod
+	def normalize_unit_costs(units):
+		'''
+		for unit in units:
+			if unit.cost < min_unit_cost:
+				unit.cost = min_unit_cost
+		'''
+		total_unit_cost_actual = sum([unit.cost for unit in units])
+		ratio = total_unit_cost / total_unit_cost_actual
+		extra_to_remove = 0.0
+		if ratio > 0.999 and ratio < 1.001:
+			return
+		for unit in units:
+			new_cost = unit.cost * ratio
 			if new_cost < min_unit_cost:
+				extra_to_remove = min_unit_cost - new_cost 
 				new_cost = min_unit_cost
-			extra_to_remove -= unit.cost - new_cost
+			debug_print("%.1f -> %.1f" %(unit.cost, new_cost))
+			unit.cost = new_cost
+
+		# this might be a problem portion. reducing min_unit_cost is probably in order
+		# and is probably not important for the simulation purposes anyways?
+		'''
+		while extra_to_remove > min_unit_cost:
+			unit = random.choice(units)
+			if unit.cost > min_unit_cost:
+				new_cost = unit.cost - min_unit_cost
+				if new_cost < min_unit_cost:
+					new_cost = min_unit_cost
+				extra_to_remove -= unit.cost - new_cost
+				unit.cost = new_cost
+		'''
+
+	@staticmethod
+	def calculate_unit_costs(units):
+		total_unit_uses = 0
+		for unit in units:
+			unit.cost_history.append(unit.cost)
+			unit.usage_rate_history.append(unit.uses)
+			total_unit_uses += unit.uses
+
+		debug_print (total_unit_cost)
+		debug_print (total_unit_uses)
+		for unit in units:
+			# this is what percentage of all units picked were this unit
+			ratio_of_units_picked = unit.uses / total_unit_uses
+			# should we instead be using what percentage of players picked this unit?
+			ratio_of_player_picks = unit.uses / total_players
+
+			# this ensures the sum is equal to the total_unit_cost
+			new_cost = ratio_of_units_picked * total_unit_cost
+			# but if they weren't picked because their previous cost was too high
+			# their new_cost might be way too low?
+
+			# this isn't going to sum to the correct level, is it?
+			# but I guess that's what normalization is for
+			new_cost_b = ratio_of_player_picks * wallet_initial
+
+
+			debug_print ("calculated " + str(unit.uses) + ": " + str(unit.cost) + " -> " + str(new_cost))
 			unit.cost = new_cost
 
 
-def calculate_unit_costs():
-	total_unit_uses = 0
-	for unit in units:
-		unit.cost_history.append(unit.cost)
-		unit.usage_rate_history.append(unit.uses)
-		total_unit_uses += unit.uses
-
-		# this is what percentage of all units picked were this unit
-		ratio_of_units_picked = unit.uses / total_unit_uses
-		# should we instead be using what percentage of players picked this unit?
-		ratio_of_player_picks = unit.uses / total_players
-
-		# this ensures the sum is equal to the total_unit_cost
-		new_cost = ratio_of_units_picked * total_unit_cost
-		# but if they weren't picked because their previous cost was too high
-		# their new_cost might be way too low?
-
-		unit.cost = new_cost
 
 
 class Player:
@@ -93,34 +118,26 @@ class Player:
 		self.unit_valuations = []
 		for unit in units:
 			valuation = unit.value
-			archetype = archetypes[unit.archetype]
-			roll = random.randrange(1.0)
+			archetype = Unit.archetypes[unit.archetype]
+			roll = random.random()
 			if roll < archetype[0]:
 				# this player underestimates the unit
-				valuation = random.randrange(0.1, 0.8) * unit.value
+				valuation = random.uniform(0.1, 0.8) * unit.value
 				if (valuation < min_unit_cost):
 					valuation = min_unit_cost
 			elif roll < archetype[1] + archetype[0]:
 				# this player correctly evaluates the unit
-				valuation = random.randrange(0.9,1.1) * unit.value
+				valuation = random.uniform(0.9,1.1) * unit.value
 			else:
 				# this player overestimates the unit
-				valuation = random.randrange(1.5, 2.5) * unit.value
+				valuation = random.uniform(1.5, 2.5) * unit.value
 				if (valuation > max_unit_valuation):
 					valuation = max_unit_valuation
-			unit_valuations.append(valuation)
+			self.unit_valuations.append(valuation)
 
-
-players = [ Player(id=id) for id in range(total_players)]
-
-
-normalize_unit_costs()
-for season in range(100):
-	for unit in units:
-		unit.uses = 0
-	for player in players:
-		player.wallet = wallet_initial
-		player.unit_set = []
+	def pick_unit_set(self, units):
+		self.wallet = wallet_initial
+		self.unit_set = []
 		unit_choices = [unit.id for unit in units]
 		# this is where we would put actual player choice if we had it
 		# maybe ordering by difference between cost and player's valuation
@@ -128,13 +145,42 @@ for season in range(100):
 		random.shuffle(unit_choices)
 		for unit_id in unit_choices:
 			unit = units[unit_id]
-			if (unit.cost > player.wallet):
+			if (unit.cost > self.wallet):
 				continue
-			player.wallet -= unit.cost
-			player.unit_set.append(unit_id)
+			self.wallet -= unit.cost
+			self.unit_set.append(unit_id)
 
-			units[unit_id].uses++
+			units[unit_id].uses += 1
 
-		player.unit_set_history.append(player.unit_set)
+		self.unit_set_history.append(self.unit_set)
+
+
+units = [ Unit(id=id) for id in range(total_units)]
+players = [ Player(id=id) for id in range(total_players)]
+Unit.normalize_unit_costs(units)
+for season in range(seasons):
+	for unit in units:
+		unit.uses = 0
+	for player in players:
+		player.pick_unit_set(units)
+	for unit in units:
+		debug_print(unit.uses)
+	Unit.calculate_unit_costs(units)
+	Unit.normalize_unit_costs(units)
+
+row = "Value\tArchetype\tUses\t"
+for season in range(seasons_to_print):
+	row += ("%d" % season) + "\t"
+row += "Final"
+print(row)
+for unit in units:
+	row = "%.1f\t%s\t%d\t" % (
+		unit.value,
+		unit.archetype.ljust(14, ' '),
+		sum(unit.usage_rate_history))
+	for season in range(seasons_to_print):
+		row += ("%.1f" % unit.cost_history[season]) + "\t"
+	row += "%.1f" % unit.cost
+	print(row)
 
 
