941b6e23996103792ca7adc2c30cccc063be11a1
Robinson Farrar <robinson.farrar@gmail.com>
2019-12-23 12:58:50 -0500

    player valuation is in, but it's causing players to swing too hard

--- experiments/deckbuilding_metagame_market.py
+++ experiments/deckbuilding_metagame_market.py
@@ -4,15 +4,20 @@ import random
 
 wallet_initial = 100.0
 total_unit_cost = 500.0
-total_units = 25
+total_units = 50
 total_players = 1000
+max_team_size = 15
 
-seasons = 100
-seasons_to_print = 1
+seasons = 10
+seasons_to_print = 10
 
 min_unit_cost = 1.0
-max_starting_unit_cost = 35.0
-max_unit_valuation = 75.0
+average_unit_cost = total_unit_cost / total_units
+
+average_team_size = wallet_initial / average_unit_cost
+
+max_starting_unit_cost = average_unit_cost * 3.5
+max_unit_valuation = wallet_initial * .8
 
 def debug_print(value):
 	#print(value)
@@ -35,8 +40,9 @@ class Unit:
 
 	def __init__(self, id):
 		self.id = id
-		self.archetype = random.choice(list(Unit.archetypes.keys()))
-		self.value = random.gauss(10.0, 5.0)
+		# archetype testing should come after testing player valuation
+		self.archetype = "Basic"#random.choice(list(Unit.archetypes.keys()))
+		self.value = random.gauss(average_unit_cost + min_unit_cost, average_unit_cost / 2)
 		self.cost = random.uniform(min_unit_cost, max_starting_unit_cost)
 		if (self.value < min_unit_cost):
 			self.value = min_unit_cost
@@ -46,11 +52,9 @@ class Unit:
 
 	@staticmethod
 	def normalize_unit_costs(units):
-		'''
 		for unit in units:
 			if unit.cost < min_unit_cost:
 				unit.cost = min_unit_cost
-		'''
 		total_unit_cost_actual = sum([unit.cost for unit in units])
 		ratio = total_unit_cost / total_unit_cost_actual
 		extra_to_remove = 0.0
@@ -59,7 +63,7 @@ class Unit:
 		for unit in units:
 			new_cost = unit.cost * ratio
 			if new_cost < min_unit_cost:
-				extra_to_remove = min_unit_cost - new_cost 
+				extra_to_remove = min_unit_cost - new_cost
 				new_cost = min_unit_cost
 			debug_print("%.1f -> %.1f" %(unit.cost, new_cost))
 			unit.cost = new_cost
@@ -142,7 +146,9 @@ class Player:
 		# this is where we would put actual player choice if we had it
 		# maybe ordering by difference between cost and player's valuation
 		# but it's still fine to pick greedily
-		random.shuffle(unit_choices)
+		#random.shuffle(unit_choices)
+		unit_choices = sorted(unit_choices, key=(lambda id: self.unit_valuations[id] - units[id].cost), reverse=True)
+
 		for unit_id in unit_choices:
 			unit = units[unit_id]
 			if (unit.cost > self.wallet):
@@ -151,6 +157,8 @@ class Player:
 			self.unit_set.append(unit_id)
 
 			units[unit_id].uses += 1
+			if len(self.unit_set) > max_team_size:
+				break
 
 		self.unit_set_history.append(self.unit_set)
 
@@ -168,19 +176,20 @@ for season in range(seasons):
 	Unit.calculate_unit_costs(units)
 	Unit.normalize_unit_costs(units)
 
-row = "Value\tArchetype\tUses\t"
+total_unit_value = sum([unit.value for unit in units])
+print("Total Value: " + str(total_unit_value))
+row = "Value\tArchetype\tFinal\tUses\t"
 for season in range(seasons_to_print):
 	row += ("%d" % season) + "\t"
-row += "Final"
 print(row)
 for unit in units:
-	row = "%.1f\t%s\t%d\t" % (
+	row = "%.1f\t%s\t%.1f\t%d\t" % (
 		unit.value,
 		unit.archetype.ljust(14, ' '),
+		unit.cost,
 		sum(unit.usage_rate_history))
 	for season in range(seasons_to_print):
-		row += ("%.1f" % unit.cost_history[season]) + "\t"
-	row += "%.1f" % unit.cost
+		row += ("%.1f" % unit.cost_history[season]).rjust(4, ' ') + "\t"
 	print(row)
 
 
