57d11b2eb5c03d2e9f24adee55852fb533e71889
Robinson Farrar <robinson.farrar@gmail.com>
2019-12-28 21:26:19 -0500

    comparing adjustment equations
    a has more overshoot and converges slower (given current player behavior), so b is preferable
    
    sorting is obviously much faster when all units start from the same value, rather than random values

--- experiments/deckbuilding_metagame_market.py
+++ experiments/deckbuilding_metagame_market.py
@@ -1,6 +1,11 @@
 from __future__ import division
 from collections import namedtuple
 import random
+import copy
+
+import plotly.express as px
+import plotly.io as pio
+import pandas as pd
 
 wallet_initial = 100.0
 total_unit_cost = 500.0
@@ -20,10 +25,15 @@ max_starting_unit_cost = average_unit_cost * 3.5
 max_unit_valuation = wallet_initial * .8
 
 adjustment_seasons = 5
-starting_lerp_value = 0.25
+starting_lerp_value = 0.15
 target_lerp_value = 0.11
 
+adjustment_method = "b"
+
+#log = open("log.txt","w+")
+
 def debug_print(value):
+	#log.write(str(value) + "\n")
 	#print(value)
 	pass
 
@@ -49,9 +59,10 @@ class Unit:
 	def __init__(self, id):
 		self.id = id
 		# archetype testing should come after testing player valuation
-		self.archetype = "Basic"#random.choice(list(Unit.archetypes.keys()))
+		self.archetype = random.choice(list(Unit.archetypes.keys()))
 		self.value = random.gauss(average_unit_cost + min_unit_cost, average_unit_cost / 2)
 		self.cost = random.uniform(min_unit_cost, max_starting_unit_cost)
+		self.cost = lerp(self.cost, 10.0, 0.6)
 		if (self.value < min_unit_cost):
 			self.value = min_unit_cost
 		self.uses = 0
@@ -120,19 +131,25 @@ class Unit:
 
 			# does averaging help out at all? is this just a crutch or necessary?
 			lerp_value = target_lerp_value
-			if (season < adjustment_seasons):
-				lerp_value = lerp(starting_lerp_value, target_lerp_value, season/adjustment_seasons)
-			new_cost = lerp(unit.cost, new_cost_b, lerp_value)
+			if season < adjustment_seasons:
+				lerp_value = lerp(
+					starting_lerp_value,
+					target_lerp_value,
+					season/adjustment_seasons)
+
+			debug_print("a: %.1f b: %.1f" % (new_cost_a, new_cost_b))
+			if adjustment_method == "a":
+				new_cost = lerp(unit.cost, new_cost_a, lerp_value)
+			elif adjustment_method == "b":
+				new_cost = lerp(unit.cost, new_cost_b, lerp_value)
 
 			debug_print ("calculated " + str(unit.uses) + ": " + str(unit.cost) + " -> " + str(new_cost))
 
 			unit.cost = new_cost
 
 
-
-
 class Player:
-	def __init__(self, id):
+	def __init__(self, id, units):
 		self.id = id
 		self.wallet = wallet_initial
 		self.unit_set = []
@@ -181,39 +198,119 @@ class Player:
 		self.unit_set_history.append(self.unit_set)
 
 
+
+
+class Simulation:
+	def __init__(self, units = None, players = None, adjustment_method_override = None):
+		# messy, but okay for now
+		global adjustment_method
+		
+		self.units = units or [ Unit(id=id) for id in range(total_units)]
+		self.players = players or [ Player(id=id, units=self.units) for id in range(total_players)]
+		self.total_unit_value = sum([unit.value for unit in self.units])
+		
+		adjustment_method = adjustment_method_override or adjustment_method
+		debug_print("adjustment " + adjustment_method)
+
+	def run(self):
+		Unit.normalize_unit_costs(self.units)
+		for season in range(seasons):
+			for unit in self.units:
+				unit.uses = 0
+			for player in self.players:
+				player.pick_unit_set(self.units)
+			for unit in self.units:
+				debug_print(unit.uses)
+			Unit.calculate_unit_costs(self.units, season)
+			Unit.normalize_unit_costs(self.units)
+
+	def log(self):
+		print("Total Value: " + str(self.total_unit_value))
+		row = "Value\tArchetype\tFinal\tUses\t"
+		for season in range(seasons_to_print):
+			row += ("%d" % season) + "\t"
+		print(row)
+		for unit in self.units:
+			row = "%.1f\t%s\t%.1f\t%d\t" % (
+				unit.value,
+				unit.archetype.ljust(14, ' '),
+				unit.cost,
+				sum(unit.usage_rate_history))
+			for season in range(seasons_to_print):
+				row += ("%.1f" % unit.cost_history[season]).rjust(4, ' ') + "\t"
+			print(row)
+
+	def graph(self, file_name):
+		global adjustment_method
+
+		file_name = file_name or "./market_output.html"
+
+		data = {
+			"season": [],
+			"cost": [],
+			"unit_id": [],
+			"value": [],
+			"type": [],
+			"uses": []
+		}
+
+		def append_data(unit_id, season, cost, uses):
+			data["season"].append(season)
+			data["cost"].append(cost)
+			data["unit_id"].append(unit_id)
+			data["value"].append(self.units[unit_id].value)
+			data["type"].append(self.units[unit_id].archetype)
+			data["uses"].append(uses)
+
+		for unit_id in range(total_units):
+			unit = self.units[unit_id]
+			for season in range(seasons_to_print):
+				append_data(
+					unit_id,
+					season,
+					unit.cost_history[season],
+					unit.usage_rate_history[season])
+			append_data(
+				unit_id,
+				seasons_to_print,
+				unit.cost,
+				unit.uses)
+
+		fig = px.line(
+			data_frame=pd.DataFrame(data),
+			x="season",
+			y="cost",
+			line_group="unit_id",
+			color="unit_id",
+			hover_data=["value", "type", "uses"],
+			title="Unit Cost Adjustment " + adjustment_method 
+		)
+
+		pio.write_html(fig, file_name, include_plotlyjs='cdn')
+
+
+sim = Simulation()
+sim.run()
+sim.graph("./market_output.html")
+
+'''
 units = [ Unit(id=id) for id in range(total_units)]
 players = [ Player(id=id) for id in range(total_players)]
-Unit.normalize_unit_costs(units)
-for season in range(seasons):
-	for unit in units:
-		unit.uses = 0
-	for player in players:
-		player.pick_unit_set(units)
-	for unit in units:
-		debug_print(unit.uses)
-	Unit.calculate_unit_costs(units, season)
-	Unit.normalize_unit_costs(units)
 
-total_unit_value = sum([unit.value for unit in units])
-print("Total Value: " + str(total_unit_value))
-row = "Value\tArchetype\tFinal\tUses\t"
-for season in range(seasons_to_print):
-	row += ("%d" % season) + "\t"
-print(row)
-for unit in units:
-	row = "%.1f\t%s\t%.1f\t%d\t" % (
-		unit.value,
-		unit.archetype.ljust(14, ' '),
-		unit.cost,
-		sum(unit.usage_rate_history))
-	for season in range(seasons_to_print):
-		row += ("%.1f" % unit.cost_history[season]).rjust(4, ' ') + "\t"
-	print(row)
+units2 = copy.deepcopy(units)
+players2 = copy.deepcopy(players)
+
+simA = Simulation(units, players, "a")
+simA.run()
+simA.graph("./market_output.html")
 
 
-import plotly.express as px
-import plotly.io as pio
-import pandas as pd
+
+simB = Simulation(units2, players2, "b")
+simB.run()
+simB.graph("./market_output_b.html")
+
+'''
 
 '''
 df = px.data.gapminder()
@@ -221,35 +318,5 @@ df = px.data.gapminder()
 fig = px.line(df, x="year", y="lifeExp", color="continent", line_group="country", hover_name="country",
         line_shape="spline", render_mode="svg")
 '''
-data = {
-	"season": [],
-	"cost": [],
-	"unit_id": [],
-	"value": []
-}
 
-def append_data(unit_id, season, cost, value):
-	data["season"].append(season)
-	data["cost"].append(cost)
-	data["unit_id"].append(unit_id)
-	data["value"].append(value)
-
-for unit_id in range(total_units):
-	unit = units[unit_id]
-	for season in range(seasons_to_print):
-		append_data(unit_id, season, unit.cost_history[season], unit.value)
-	append_data(unit_id, seasons_to_print, unit.cost, unit.value)
-	#append_data(unit_id, seasons_to_print+1, unit.value)
-
-fig = px.line(
-	data_frame=pd.DataFrame(data),
-	x="season",
-	y="cost",
-	line_group="unit_id",
-	color="unit_id",
-	hover_data=["value"],
-	title="Unit Cost Adjustment"
-)
-
-pio.write_html(fig, "./market_output.html", include_plotlyjs='cdn')
 
