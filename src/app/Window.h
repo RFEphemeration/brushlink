641544568e4de86d461cfc9ecad8cf9f42bdde9f
Robinson Farrar <robinson.farrar@gmail.com>
2020-06-17 17:46:50 -0400

    includes

--- src/app/Window.h
+++ src/app/Window.h
@@ -2,10 +2,20 @@
 #ifndef BRUSHLINK_WINDOW_H
 #define BRUSHLINK_WINDOW_H
 
-#include "tigr.h"
+#include <memory>
+#include <string>
+#include <string_view>
+
+#include "TigrExtensions.h"
 
 namespace Brushlink
 {
+
+struct Key_Changes
+{
+	std::string_view up_values;
+	std::string_view down_values;
+};
 
 struct Window_Settings
 {
@@ -20,6 +30,8 @@ struct Window
 {
 	Window_Settings settings;
 	shared_ptr<Tigr> screen;
+	char key_down_buffer[256];
+	char key_up_buffer[256];
 
 	Window(Window_Settings settings = Window_Settings{})
 		: settings{settings}
@@ -33,19 +45,32 @@ struct Window
 		));
 	}
 
-	bool Closed()
+	inline bool Closed()
 	{
 		return tigrClosed(screen.get());
 	}
 
-	void Clear()
+	inline void Clear()
 	{
 		tigrClear(screen.get(), settings.clear_color);
 	}
 
-	void Present()
+	inline void PresentAndUpdate()
 	{
 		tigrUpdate(screen.get());
+	}
+
+	inline Key_Changes GetKeyChanges()
+	{
+		int down_count = 0;
+		int up_count = 0;
+		tigrGetKeyChanges(screen.get(),
+			&key_down_buffer, &down_count,
+			&key_up_buffer, &up_count);
+		return Key_Changes {
+			{ key_down_buffer, down_count },
+			{ key_up_buffer, up_count }
+		};
 	}
 };
 
