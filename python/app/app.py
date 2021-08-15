15dfa310f10030b17282f853672bf5308b4678ef
Robinson Farrar <robinson.farrar@gmail.com>
2021-08-15 16:09:08 -0400

    reorganizing app screens, starting popups

--- python/app/app.py
+++ python/app/app.py
@@ -6,13 +6,10 @@ pyglet.resource.path = [os.path.dirname(__file__) + '/resources/']
 pyglet.resource.reindex()
 
 class App():
-	def __init__(self, initial, screens):
+	def __init__(self, initial):
 		self.screens = {}
-		for screen in screens:
-			screen.set_app(self);
-			self.screens[screen.name] = screen
 		self.elapsed_time = 0.0
-		self.change_state(initial)
+		self.current_screen = initial
 
 	def draw(self):
 		self.screens[self.current_screen].draw()
@@ -28,12 +25,25 @@ class App():
 	def on_mouse_press(self, x, y, buttons, modifiers):
 		self.screens[self.current_screen].on_mouse_press(x, y, buttons, modifiers)
 
+	def exit(self):
+		pyglet.app.exit()
+
+	def delete_screens(self):
+		self.screens = {}
+
+	def add_screen(self, screen):
+		self.screens[screen.name] = screen
+		screen.set_app(self)
+		if screen.name == self.current_screen:
+			screen.on_enter()
+
+
 class Screen():
-	def __init__(self, name, wigits, exit_time = None, exit_state = None):
+	def __init__(self, name, wigits, exit_time = None, on_exit = None):
 		self.name = name
 		self.wigits = wigits
 		self.exit_time = exit_time
-		self.exit_state = exit_state
+		self.on_exit = on_exit
 		self.elapsed_time = 0.0
 		self.app = None
 
@@ -48,8 +58,8 @@ class Screen():
 
 	def update(self, dt):
 		self.elapsed_time += dt
-		if self.exit_time and self.elapsed_time >= self.exit_time:
-			app.change_state(self.exit_state)
+		if self.exit_time is not None and self.elapsed_time >= self.exit_time:
+			self.on_exit(self)
 
 	def draw(self):
 		for wigit in self.wigits:
@@ -76,6 +86,36 @@ class MatchScreen():
 		glPopMatrix()
 
 
+class Popup(pyglet.gui.WidgetBase):
+	def __init__(self, name, wigits, x, y, color=(100,100,100), width=400, height=600, active=False):
+		self.background = pyglet.shapes.Rectangle(x=x, y=y, width=width, height=height, color=color)
+		self.background.anchor_x = width // 2;
+		self.background.anchor_y = height // 2;
+		self.name = name
+		self.wigits = wigits
+		self.active = active
+
+	def draw(self):
+		if not self.active:
+			return
+		self.background.draw()
+		for wigit in self.wigits:
+			wigit.draw()
+
+	def on_mouse_press(self, x, y, buttons, modifiers):
+		if not self.active:
+			return
+		for wigit in self.wigits:
+			if getattr(wigit, 'on_mouse_press', None):
+				wigit.on_mouse_press(x, y, buttons, modifiers)
+
+	def show(self):
+		self.active = True
+
+	def hide(self):
+		self.active = False
+
+
 class Button(pyglet.gui.WidgetBase):
 	def __init__(self, label, x, y, on_press,font_size = 18, color=(155,155,255), width=200, height=40):
 		super().__init__(x - width // 2, y - height // 2, width, height);
@@ -83,14 +123,6 @@ class Button(pyglet.gui.WidgetBase):
 		self.background.anchor_x = width // 2;
 		self.background.anchor_y = height // 2;
 		self.label = pyglet.text.Label(label, font_size, x=x, y=y, anchor_x='center', anchor_y='center');
-		'''
-		depressed = pyglet.resource.image('button_up.png')
-		pressed = pyglet.resource.image('button_down.png')
-
-		texture = pyglet.image.Texture.create(width, height, rectangle=True)
-		self.button = pyglet.gui.PushButton(x, y, pressed=texture, depressed=texture)
-		self.button.set_handler('on_press', lambda: on_press(self))
-		'''
 		self.on_press = on_press
 		self.app = None
 
@@ -108,8 +140,11 @@ class Button(pyglet.gui.WidgetBase):
 
 window = pyglet.window.Window(fullscreen=True)
 
-app = App(initial="title", screens=[
-	Screen("title", [
+app = App(initial='title')
+
+def init_screens(app):
+	screens = {}
+	app.add_screen(Screen('title', [
 		pyglet.text.Label(
 			'BrushLink',
 			font_size=36,
@@ -124,8 +159,10 @@ app = App(initial="title", screens=[
 			y=window.height // 2 - 20,
 			anchor_x='center',
 			anchor_y='center'),
-		], exit_time=0.5, exit_state="main"),
-	Screen("main", [
+		],
+		exit_time=0.5,
+		on_exit=lambda s: s.app.change_state('main')))
+	app.add_screen(Screen('main', [
 		pyglet.text.Label(
 			'BrushLink',
 			font_size=36,
@@ -134,22 +171,55 @@ app = App(initial="title", screens=[
 			anchor_x='center',
 			anchor_y='center'),
 		Button(
-			'settings',
+			"settings",
 			x=window.width // 2,
 			y=window.height - 100,
-			on_press=lambda b: b.app.change_state("settings")),
-		]),
-	Screen("settings", [
+			on_press=lambda b: b.app.change_state('settings')),
+		Button(
+			"play",
+			x=window.width // 2,
+			y=window.height - 150,
+			on_press=lambda b: b.app.change_state('game')),
+		Button(
+			"exit",
+			x=window.width // 2,
+			y=window.height - 200,
+			on_press=lambda b: b.app.change_state('exit')),
+		]))
+	app.add_screen(Screen('settings', [
 		pyglet.text.Label(
-			'Settings',
+			"Settings",
 			font_size=36,
 			x=window.width // 2,
 			y=window.height - 40,
 			anchor_x='center',
 			anchor_y='center'),
-		]),
-	])
+		Button(
+			'back',
+			x=window.width // 2,
+			y=window.height - 100,
+			on_press=lambda b: b.app.change_state('main')),
+		]))
+	game_menu = Popup('menu', [
+		Button(
+			"menu",
+			x=window.width - 130,
+			y=50,
+			on_press=lambda b: b.screen.show_popup('menu')),
+		])
+	app.add_screen(Screen('game', [
+		Button(
+			"menu",
+			x=window.width - 130,
+			y=50,
+			on_press=lambda b: b.screen.show_popup('menu')),
+		]))
+	app.add_screen(Screen('exit', [],
+		exit_time=0.0,
+		on_exit=lambda s: s.app.exit()
+	))
 
+init_screens(app)
 
 @window.event
 def on_draw():
